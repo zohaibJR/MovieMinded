339aedb886ce85cc90ab9d4f2e7c0de7a065d480
zohaibJR <zohaibrehman595@gmail.com>
2025-04-21 20:16:43 +0500

    Now want to display items from db

--- moviebase/src/Pages/AddMovie.jsx
+++ moviebase/src/Pages/AddMovie.jsx
@@ -45,55 +45,65 @@ const AddMovie = () => {
 
   return (
     <div className="form-container">
-      <h1>Add Movie Form</h1>
-      <form onSubmit={handleSubmit} encType="multipart/form-data">
-        <label>Movie Name:
-          <input type="text" name="moviename" value={formData.moviename} onChange={handleChange} required />
-        </label>
+  <form onSubmit={handleSubmit} encType="multipart/form-data">
 
-        <label>Summary:
-          <textarea name="summary" value={formData.summary} onChange={handleChange} required />
+    <div className="leftSide">
+        <label>Upload Picture:
+            <input type="file" name="picture" accept="image/*" onChange={handleChange} />
         </label>
-
-        <label>Youtube Link:
-          <input type="text" name="youtubeLink" value={formData.youtubeLink} onChange={handleChange} required />
-        </label>
-
-        <label>Release Year:
-          <input type="number" name="releaseYear" value={formData.releaseYear} onChange={handleChange} required />
-        </label>
-
-        <label>Genre:
-          <input type="text" name="genre" value={formData.genre} onChange={handleChange} required />
-        </label>
-
-        <label>Duration (min):
-          <input type="number" name="duration" value={formData.duration} onChange={handleChange} required />
-        </label>
-
-        <label>Language:
-          <input type="text" name="language" value={formData.language} onChange={handleChange} required />
-        </label>
-
-        <label>Country:
-          <input type="text" name="country" value={formData.country} onChange={handleChange} required />
-        </label>
-
-        <label>Age Rating:
-          <input type="number" step="0.1" name="ageRating" value={formData.ageRating} onChange={handleChange} required />
-        </label>
-
-        <label>PG Rating:
-          <input type="text" name="pgRating" value={formData.pgRating} onChange={handleChange} required />
-        </label>
-
-        <label>Picture:
-          <input type="file" name="picture" onChange={handleChange} accept="image/*" required />
-        </label>
-
-        <button type="submit">Add Movie</button>
-      </form>
     </div>
+
+    <div className="midSide">
+      <h1>Mid Side</h1>
+        <label>Movie Name:
+            <input type="text" name="moviename" value={formData.moviename} onChange={handleChange} />
+          </label>
+          <br />
+
+          <label>Summary:
+            <textarea name="summary" value={formData.summary} onChange={handleChange} />
+          </label>
+
+          <label>Release Year:
+            <input type="number" name="releaseYear" value={formData.releaseYear} onChange={handleChange} />
+          </label>
+
+          <label>Genre:
+            <input type="text" name="genre" value={formData.genre} onChange={handleChange} />
+          </label>
+
+          <label>Duration (mins):
+            <input type="number" name="duration" value={formData.duration} onChange={handleChange} />
+          </label>
+    </div>
+
+    <div className="rightSide">
+      <h1>Right Side</h1>
+      <label>Language:
+            <input type="text" name="language" value={formData.language} onChange={handleChange} />
+          </label>
+
+          <label>Country:
+            <input type="text" name="country" value={formData.country} onChange={handleChange} />
+          </label>
+
+          <label>Age Rating:
+            <input type="text" name="ageRating" value={formData.ageRating} onChange={handleChange} />
+          </label>
+
+          <label>PG Rating:
+            <input type="text" name="pgRating" value={formData.pgRating} onChange={handleChange} />
+          </label>
+
+          <label>YouTube Link:
+            <input type="text" name="youtubeLink" value={formData.youtubeLink} onChange={handleChange} />
+          </label>
+
+          <button type="submit">Add Movie</button>
+    </div>
+
+  </form>
+</div>
   );
 };
 
