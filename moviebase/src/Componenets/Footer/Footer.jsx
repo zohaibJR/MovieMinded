676079e6a132c2c271db3c72b6935846422ef15c
zohaibJR <zohaibrehman595@gmail.com>
2025-04-29 18:26:39 +0500

    All Set with Footer

--- moviebase/src/Componenets/Footer/Footer.jsx
+++ moviebase/src/Componenets/Footer/Footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import "../Footer/Footer.css"
+import { FaFacebook, FaInstagram, FaEnvelope } from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <div className='FooterMain'>
-        <h1>This is Footer Component</h1>
+      
+      <div className="FooterIcons">
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebook size={30} />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram size={30} />
+        </a>
+        <a href="mailto:your-email@example.com">
+          <FaEnvelope size={30} />
+        </a>
+      </div>
+
+      <h2>Copyright © 2025 MovieMinded. All rights reserved.</h2>
+
     </div>
   )
 }
